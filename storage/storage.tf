6b92d421a7f75a23018a424e1e938914aee5b75c
jay <india.jai@gmail.com>
2025-03-07 20:37:29 +0000

    test

--- storage/storage.tf
+++ storage/storage.tf
@@ -1,9 +1,12 @@
-
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
 
 resource "azurerm_storage_account" "mystorageaccount" {
   name                     = var.storage_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.resource_group_location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
