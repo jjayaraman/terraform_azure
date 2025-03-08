991029f3b92743a942f1a7f6661e1da38f6b252b
jay <india.jai@gmail.com>
2025-03-08 15:32:32 +0000

    tf state setup

--- tf_setup/existing.tf
+++
@@ -1,3 +0,0 @@
-data "azurerm_resource_group" "tf_state_rg_existing" {
-  name = var.resource_group_name
-}
--- tf_setup/tf_state_storage.tf
+++ tf_setup/tf_state_storage.tf
@@ -1,13 +1,12 @@
 resource "azurerm_resource_group" "tf_state_rg" {
-  count    = length(data.azurerm_resource_group.tf_state_rg_existing.name) > 0 ? 0 : 1 // if resource group does not exist create
   name     = var.resource_group_name
   location = var.resource_group_location
 }
 
 resource "azurerm_storage_account" "tf_state_storage" {
   name                     = "${var.storage_name_prefix}${var.environment}"
-  resource_group_name      = length(azurerm_resource_group.tf_state_rg) > 0 ? azurerm_resource_group.tf_state_rg[0].name : data.azurerm_resource_group.tf_state_rg_existing.name
-  location                 = length(azurerm_resource_group.tf_state_rg) > 0 ? azurerm_resource_group.tf_state_rg[0].location : data.azurerm_resource_group.tf_state_rg_existing.location
+  resource_group_name      = azurerm_resource_group.tf_state_rg.name
+  location                 = azurerm_resource_group.tf_state_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
