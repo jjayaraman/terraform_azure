63647a6cc6056fa17c3f3f68d839134537c8fd55
jay <india.jai@gmail.com>
2025-03-09 07:38:36 +0000

    name change

--- storage/storage.tf
+++ storage/storage.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
 }
 
-resource "azurerm_storage_account" "tf_state_storage" {
+resource "azurerm_storage_account" "my_storage" {
   name                     = var.storage_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
@@ -13,8 +13,8 @@ resource "azurerm_storage_account" "tf_state_storage" {
   public_network_access_enabled = false
 }
 
-resource "azurerm_storage_container" "tf_state_storage_container" {
-  name                  = "tfstate"
-  storage_account_id    = azurerm_storage_account.tf_state_storage.id
+resource "azurerm_storage_container" "my_storage_container" {
+  name                  = "mycontainer"
+  storage_account_id    = azurerm_storage_account.my_storage.id
   container_access_type = "private"
 }
